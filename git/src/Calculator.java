dea2b970d0191f1fa25b4b0b0fa5c5f40b837465
Dasha Butvilovskaya <butvilovskayadasha@gmail.com>
2018-09-12 20:15:18 +0300

    Added function to display results to the console

--- git/src/Calculator.java
+++ git/src/Calculator.java
@@ -3,6 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Calculator {
+
+    static void printResult(double num1, double num2, double num3, String operation) {
+        System.out.println("Result: " + num1 + " " + operation + " " + num2 + " = " + num3);
+    }
+
     public static void main(String[] args) {
         double num1 = 0, num2 = 0, num3 = 0;
         String operation = "";
@@ -26,28 +31,28 @@ public class Calculator {
 
         switch (operation) {
             case "+": num3 = num1 + num2;
-                System.out.println("Result: " + num1 + " " + operation + " " + num2 + " = " + num3);
+                printResult(num1, num2, num3, operation);
                 break;
             case "-": num3 = num1 - num2;
-                System.out.println("Result: " + num1 + " " + operation + " " + num2 + " = " + num3);
+                printResult(num1, num2, num3, operation);
                 break;
             case "*": num3 = num1 * num2;
-                System.out.println("Result: " + num1 + " " + operation + " " + num2 + " = " + num3);
+                printResult(num1, num2, num3, operation);
                 break;
             case "/": num3 = num1 / num2;
-                System.out.println("Result: " + num1 + " " + operation + " " + num2 + " = " + num3);
+                printResult(num1, num2, num3, operation);
                 break;
             case "%": num3 = num1 % num2;
-                System.out.println("Result: " + num1 + " " + operation + " " + num2 + " = " + num3);
+                printResult(num1, num2, num3, operation);
                 break;
             case "^": num3 = Math.pow(num1, num2);
-                System.out.println("Result: " + num1 + " " + operation + " " + num2 + " = " + num3);
+                printResult(num1, num2, num3, operation);
                 break;
             case "min": num3 = Math.min(num1, num2);
-                System.out.println("Result: " + num1 + " " + operation + " " + num2 + " = " + num3);
+                printResult(num1, num2, num3, operation);
                 break;
             case "max": num3 = Math.max(num1, num2);
-                System.out.println("Result: " + num1 + " " + operation + " " + num2 + " = " + num3);
+                printResult(num1, num2, num3, operation);
                 break;
             default: System.out.println("Invalid operation");
         }
