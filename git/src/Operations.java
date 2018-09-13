386aee942f9222d2b7f0b7ef84607cff3181594a
Dasha Butvilovskaya <butvilovskayadasha@gmail.com>
2018-09-13 21:41:45 +0300

    Created a new class for operations

---
+++ git/src/Operations.java
@@ -0,0 +1,34 @@
+public class Operations {
+
+    public static double addition(double n1, double n2) {
+        return n1 + n2;
+    }
+
+    public static double subtraction(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    public static double multiplication(double n1, double n2) {
+        return n1 * n2;
+    }
+
+    public static double division(double n1, double n2) {
+        return n1 / n2;
+    }
+
+    public static double remainder(double n1, double n2) {
+        return n1 % n2;
+    }
+
+    public static double exponentiation(double n1, double n2) {
+        return Math.pow(n1, n2);
+    }
+
+    public static double minimum(double n1, double n2) {
+        return Math.min(n1, n2);
+    }
+
+    public static double maximum(double n1, double n2) {
+        return Math.max(n1, n2);
+    }
+}
