7e55b65708b0c10718a0649a7a56ce3c6c33c9f6
Dasha Butvilovskaya <butvilovskayadasha@gmail.com>
2018-09-13 21:55:27 +0300

    Created a new function sqrt

--- git/src/Operations.java
+++ git/src/Operations.java
@@ -31,4 +31,8 @@ public class Operations {
     public static double maximum(double n1, double n2) {
         return Math.max(n1, n2);
     }
+
+    public static double sqrt(double n1) {
+        return Math.sqrt(n1);
+    }
 }
